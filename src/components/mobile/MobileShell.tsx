d705ddc3919db2eed4196c9831295a98a02adf24
Nicholas Kerr <nicholas_kerr1990@yahoo.com>
2026-05-08 13:20:38 -0400

    Reset tab scroll instantly

--- src/components/mobile/MobileShell.tsx
+++ src/components/mobile/MobileShell.tsx
@@ -20,7 +20,7 @@ export function MobileShell() {
   const travelOS = useTravelOS();
 
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
   }, [activeTab]);
 
   useEffect(() => {
